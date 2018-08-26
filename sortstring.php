5c6a732c6420952b4f17f58a101492685de98efd
Andrew Belov <Fearess7@gmail.com>
2018-08-26 19:07:37 +0300

    Deleted output from sortString. Added returning result from function. Fixed useless work in algorithm.

--- sortstring.php
+++ sortstring.php
@@ -1,20 +1,25 @@
 <?php
 
+/**
+ * @param $str
+ * @return string
+ */
 function sortString($str)
 {
-    print "Результат: '";
-
+    $res_str = "";
     if(strlen($str) != 0) // if str is empty
     {
         $words = explode(" ", $str);   //separate string to words
 
         foreach ($words as $word) // word by word check
         {
-            for ($i=0; $i<strlen($word); $i++)
+            $word_len = strlen($word);  // better check it once then checking every iteration (in case of big data)
+            for ($i=0; $i<$word_len; $i++)
             {
                 if(is_numeric($word[$i])) // finding a digit
                 {
                     $res[$word] = $word[$i];
+                    break; // there is no sense to check smt after digit and we will save a lot of iteration in very large texts
                 }
             }
         }
@@ -22,16 +27,15 @@ function sortString($str)
         asort($res);
 
         foreach ($res as $key => $value)
-            print $key . " ";
-
+        {
+            $res_str .= $key . " ";
+        }
     }
-
-    print "'"; // closing "'" in the output
+    return $res_str;
 }
 
 print "Testing:<br>";
-sortString("g5et ski3lls on6 use1 your2 to4 7top");
-print "<br>";
-sortString("");
-print "<br>";
-sortString("practic3 h4rder yo1u 2hould");
+
+print "Результат: '" . sortString("g5et ski3lls on6 use1 your2 to4 7top") . "'<br>";
+print "Результат: '" . sortString("") . "'<br>";
+print "Результат: '" . sortString("practic3 h4rder yo1u 2hould") . "''";
